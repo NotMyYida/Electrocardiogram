13301feeeb00c86249f521771e372043255b3c4b
unknown <qc3@homecare.com>
2019-10-17 15:42:27 +0800

    First commit for electrocardiogram

---
+++ src/com/example/electrocardiogram/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.electrocardiogram;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		Electrocardiogram electrocardiogram 
+								= (Electrocardiogram)findViewById(R.id.electrocardiogram);
+		electrocardiogram.startDraw();
+	}
+
+	
+}
